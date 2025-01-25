62d45c138464b3bac5d16eadf901836a80e39b8e
CynicalDoggo <157577893+CynicalDoggo@users.noreply.github.com>
2025-01-25 14:02:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ key = os.getenv("SUPABASE_KEY")
 supabase = create_client(url, key)
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "https://your-firebase-app.web.app"}})
 
 #Hash Function
 def hash_password(password: str) -> str:
